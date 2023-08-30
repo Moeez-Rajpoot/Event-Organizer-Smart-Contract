8569b6ff70b6e2f66b404a6cb4fadab7f2f5f4fd
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-30 08:43:49 +0500

    Updated

--- event.sol
+++ event.sol
@@ -1,3 +1,21 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+contract EventArrange {
+
+    uint EventID;
+    mapping(uint=>Event) public EventLocation;
+    mapping(address=>mapping(uint=>uint))  public Tickets;
+
+    struct Event{
+
+        address Organizer;
+        string Event_Name;
+        
+
+
+
+    }
+
+    
+}
