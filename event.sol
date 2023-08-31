70543a06351ce6e96699e0e7a40a944950fba723
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-31 12:53:34 +0500

    Updated

--- event.sol
+++ event.sol
@@ -34,17 +34,28 @@ contract EventArrange {
 
     }
 
-    function BuyTicket(uint _Eventid , uint qunatity  ) payable  public {
+    function BuyTicket(uint _Eventid , uint quantity  ) payable  public {
         Event storage Eventvar = EventLocation[_Eventid];
          require(Eventvar.Event_Date > block.timestamp, "Tickets are Closed");
-         require(qunatity < Eventvar.Total_Tickets, "Can't buy more than Total Supply");
-         require(qunatity <= Eventvar.Remaining_Tickets ,"Not Enough Tickets");
-         require(msg.value == (Eventvar.Price * qunatity), "Not Enough Ethers");
+         require(quantity < Eventvar.Total_Tickets, "Can't buy more than Total Supply");
+         require(quantity <= Eventvar.Remaining_Tickets ,"Not Enough Tickets");
+         require(msg.value == (Eventvar.Price * quantity), "Not Enough Ethers");
 
-         
+         Eventvar.Remaining_Tickets -= quantity;
+         Tickets[msg.sender][_Eventid] += quantity;
+         address payable ORG = payable(Eventvar.Organizer);
+         ORG.transfer(msg.value);
+ 
+    }
 
+    function TransferTickets(uint _Eventid , uint quantity , address to) external  {
+        Event storage Eventvar = EventLocation[_Eventid];
+        require(Tickets[msg.sender][_Eventid]>=quantity,"You dont have enough tickets");
+        require(Eventvar.Event_Date > block.timestamp, "Tickets Purchasing are Closed now");
+        require(quantity > 0 ,"Quantity should be more than zero");
 
-
+       Tickets[msg.sender][_Eventid]-=quantity;
+       Tickets[to][_Eventid]+=quantity;
         
     }
 
